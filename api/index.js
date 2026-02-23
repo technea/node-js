5ba8ad51394bfae8ab16a8421703900e8001f51f
technea <shanikhan10009@gamil.com>
2026-02-23 19:46:04 +0500

    fix(vercel): add root fallback route in express

--- api/index.js
+++ api/index.js
@@ -13,6 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from root
+app.use(express.static(path.join(__dirname, '..')));
+
+// Root Route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
 // API Route
 app.get('/api/weather', (req, res) => {
   const city = req.query.city;
