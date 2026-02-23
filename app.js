2097c5551423c1e4dd04f08ad27be5208ac457ff
technea <shanikhan10009@gamil.com>
2026-02-23 12:53:53 +0500

    ALl weather data updated you can now check my using city name

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const https = require('https');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve index.html at root route
 app.get('/', (req, res) => {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=6160f20febb47fb50bf134c7613cfbaa&units=metric';
-  
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+app.post('/weather', (req, res) => {  
+  const city = req.body.city
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6160f20febb47fb50bf134c7613cfbaa&units=metric`
+
   https.get(url, (response) => {
     let dataChunks = '';
 
@@ -13,21 +24,32 @@ app.get('/', (req, res) => {
     });
 
     response.on('end', () => {
-      const weatherData = JSON.parse(dataChunks);
-      const temp = weatherData.main.temp;
-      const weatherDes = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      try {
+        const weatherData = JSON.parse(dataChunks);
 
-      res.send(`
-        <h1>Temperature in Lahore: ${temp}°C</h1>
-        <h2>Weather: ${weatherDes}</h2>
-        <img src="${iconUrl}" alt="Weather Icon">
-      `);
+        if (weatherData.cod !== 200) {
+          return res.status(404).send(`<h1>City not found: ${city}</h1>`);
+        }
+
+        const temp = weatherData.main.temp;
+        const weatherDes = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+        res.send(`
+          <h1>Temperature in ${city}: ${temp}°C</h1>
+          <h2>Weather: ${weatherDes}</h2>
+          <img src="${iconUrl}" alt="Weather Icon">
+        `);
+      } catch {
+        res.status(500).send('<h1>Error parsing weather data</h1>');
+      }
     });
+  }).on('error', () => {
+    res.status(500).send('<h1>Unable to fetch weather data</h1>');
   });
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on PORT 3000')
-})
+  console.log('Server is listening on PORT 3000');
+});
